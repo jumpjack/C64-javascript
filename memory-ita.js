27b24e27b15a06a9a69512695a9464ae2368ff3f
jumpjack <jumpjack@libero.it>
2024-09-23 10:19:03 +0200

    Create memory-ita.js

---
+++ memory-ita.js
@@ -0,0 +1,38 @@
+[
+  {
+    "indirizzo": "$E000-$FFFF",
+    "descrizione": "KERNAL (ROM)"
+  },
+  {
+    "indirizzo": "$D000-$DFFF",
+    "descrizione": "Characters definition (ROM) or I/O devices (RAM)"
+  },
+  {
+    "indirizzo": "$C000-$CFFF",
+    "descrizione": "Upper RAM area (free for user use)"
+  },
+  {
+    "indirizzo": "$A000-$BFFF",
+    "descrizione": "BASIC interpreter (ROM) or free space (RAM)"
+  },
+  {
+    "indirizzo": "$0800-$9FFF",
+    "descrizione": "BASIC program (RAM)"
+  },
+  {
+    "indirizzo": "$0400-$07FF",
+    "descrizione": "Text screen conents (RAM)"
+  }
+  {
+    "indirizzo": "$0200-$03FF",
+    "descrizione": "Kernal variables"
+  },
+  {
+    "indirizzo": "$0100-$01FF",
+    "descrizione": "Processor stack"
+  },
+  {
+    "indirizzo": "$0000-$00FF",
+    "descrizione": "Zero page"
+  }
+]
