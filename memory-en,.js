96b4ec24498e480e2aae3f9317488c012dde3873
jumpjack <jumpjack@libero.it>
2024-09-23 10:19:25 +0200

    Create memory-en,.js

---
+++ memory-en,.js
@@ -0,0 +1,38 @@
+[
+  {
+    "address": "$E000-$FFFF",
+    "description": "KERNAL (ROM)"
+  },
+  {
+    "address": "$D000-$DFFF",
+    "description": "Characters definition (ROM) or I/O devices (RAM)"
+  },
+  {
+    "address": "$C000-$CFFF",
+    "description": "Upper RAM area (free for user use)"
+  },
+  {
+    "address": "$A000-$BFFF",
+    "description": "BASIC interpreter (ROM) or free space (RAM)"
+  },
+  {
+    "address": "$0800-$9FFF",
+    "description": "BASIC program (RAM)"
+  },
+  {
+    "address": "$0400-$07FF",
+    "description": "Text screen contents (RAM)"
+  },
+  {
+    "address": "$0200-$03FF",
+    "description": "Kernal variables"
+  },
+  {
+    "address": "$0100-$01FF",
+    "description": "Processor stack"
+  },
+  {
+    "address": "$0000-$00FF",
+    "description": "Zero page"
+  }
+]
