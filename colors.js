2a83e960316c9beaaa9201b13d27370a939657d3
jumpjack <jumpjack@libero.it>
2024-09-24 09:41:09 +0200

    Add files via upload

---
+++ colors.js
@@ -0,0 +1,18 @@
+const colorMap = {
+  0: "#000000",   // Black
+  1: "#FFFFFF",   // White
+  2: "#880000",   // Red
+  3: "#AAFFEE",   // Cyan
+  4: "#CC44CC",   // Violet
+  5: "#00CC55",   // Green
+  6: "#0000AA",   // Blue
+  7: "#EEEE77",   // Yellow
+  8: "#DD8855",   // Orange
+  9: "#664400",   // Brown
+  10: "#FF7777",  // Light red
+  11: "#333333",  // Dark grey
+  12: "#777777",  // Grey
+  13: "#AAFF66",  // Light green
+  14: "#0088FF",  // Light blue
+  15: "#BBBBBB"   // Light grey
+};
